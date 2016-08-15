cec428d0018f84c3d736b17eac1d7f791660d183
brossetti <brossetti@nsintheta.com>
2016-08-14 21:22:53 -0400

    minor changes

--- code/spectralearner.m
+++ code/spectralearner.m
@@ -15,7 +15,7 @@ elseif nargin == 2
 end
     
 % load model or generate model
-if exist(fullfile(refdir, 'model.mat'), 'file') && 0
+if exist(fullfile(refdir, 'model.mat'), 'file')
     load(fullfile(refdir, 'model.mat'));
 else 
     [X, Y, classes] = getrefdata(refdir, true);
